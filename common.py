02e24c145de1442b13cec4e6ee50357725f1f659
徐豪 <21210240375@m.fudan.edu.cn>
2022-01-06 11:59:39 +0800

    实现员工管理系统的注册功能，提供rest api

---
+++ common.py
@@ -0,0 +1,3 @@
+EMPLOYEE_USER_TOPIC = "topic1"
+EMPLOYEE_PROPERTIES = ["number", "name", "department"]
+MSG_NEW_EMPLOYEE = "new employee|{number}|{name}|{department}"
--- employee.py
+++ employee.py
@@ -1,21 +1,65 @@
+import time
+from flask import Flask
+from flask import request
+from flask import abort
+from flask import jsonify
+from common import EMPLOYEE_USER_TOPIC
+from common import EMPLOYEE_PROPERTIES
+from common import MSG_NEW_EMPLOYEE
 from myredis.client import redis_client
 from mykafka.producer import kafka_producer
 
 
-MSG_NEW_EMPLOYEE = "new employee|{number}|{name}|{department}"
+app = Flask(__name__)
 
 
-def register(number, name, department):
-    """
-    注册一个新员工
-    :param number: 工号
-    :param name: 姓名
-    :param department: 部门
-    :return: None
-    """
-    # TODO:将新员工的信息写入redis
+def on_send_success(record_metadata, result):
+    result['success'] = True
+    result['info'] = "Successfully send message to topic {}, partition {}, offset {}".\
+        format(record_metadata.topic, record_metadata.partition, record_metadata.offset)
 
-    # TODO:将注册新员工这一事件写入kafka，用户管理系统会从kafka中读取该事件
-    #      kafka server应当事先建立一个名为employee-user并且只包含一个分区的topic，用于传递员工管理系统和用户管理系统之间的消息
-    #      消息格式为MSG_NEW_EMPLOYEE
-    #      消息构造示例: MSG_NEW_EMPLOYEE.format(number="000000", name="alice", department="sale")
+
+def on_send_fail(e, result):
+    result['success'] = False
+    result['info'] = "Fail to send message with error {}".format(e)
+
+
+def register_request_is_valid(register_request):
+    for prop in EMPLOYEE_PROPERTIES:
+        if prop not in register_request.json:
+            return False
+    return True
+
+
+@app.route('/employee/api/register', methods=['POST'])
+def register():
+    if not register_request_is_valid(request):
+        abort(400)
+
+    # 将新员工的信息写入redis的0号数据库
+    number = request.json['number']
+    name = request.json['name']
+    department = request.json['department']
+    key = number
+    value = {
+        "name": name,
+        "department": department
+    }
+    redis_client.hset(name=key, mapping=value)
+
+    # 将注册新员工这一事件写入kafka，用户管理系统会从kafka中读取该事件
+    # kafka server应当事先建立一个名为employee-user并且只包含一个partition的topic，用于传递员工管理系统和用户管理系统之间的消息
+    result = {
+        "success": False,
+        "info": ""
+    }
+    msg_new_employee = MSG_NEW_EMPLOYEE.format(number=number, name=name, department=department).encode()
+    kafka_producer.send(EMPLOYEE_USER_TOPIC, msg_new_employee).\
+        add_callback(on_send_success, result=result).\
+        add_errback(on_send_fail)
+    time.sleep(0.1)
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run()
